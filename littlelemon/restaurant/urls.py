d38dd3adb9bef7582f7bdb2eb9ead6f037c6dd5c
mustafaAIM <moustafa.alhaiba.2003@gmail.com>
2023-09-25 16:18:01 +0300

    readme.md file

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
  
     path('menu/', views.MenuItemsView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
-     path('api-token-auth/', obtain_auth_token),
+    path('api-token-auth/', obtain_auth_token),
 ]
